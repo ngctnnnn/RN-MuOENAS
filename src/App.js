3c00fa9aac43cd2f5fad3f54202ff3929b00266d
twelcone <twelcone@github.com>
2022-03-23 11:35:31 +0700

    Create a button

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {Alert, AppRegistry, StyleSheet, Text, View} from 'react-native';
+import Button from 'react-native-button';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Code base for final project CS532</Text>
-    </View>
-  );
-};
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  hello: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
+    hello: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    button: {
+        fontSize: 20,
+        padding: 10,
+        color: 'white',
+        backgroundColor: 'green',
+        borderRadius: 10
+    }
+  });
 
-AppRegistry.registerComponent(
-  'MyReactNativeApp',
-  () => App
-);
+export default class App extends Component {
+    _onPressButton = () => (
+        Alert.alert("U pressed the button !")
+    )
+    render() {
+        return(<View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
 
-export default App;
+        <Text style={styles.hello}>Code base for CS523 project</Text>
+
+        <Button style={styles.button}
+        onPress={() => { navi}}
+        >This is a button</Button>
+
+        </View>);
+    }
+}
+
+
+AppRegistry.registerComponent('Test', () => App)
